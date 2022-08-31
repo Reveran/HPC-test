020f056d5da82d11da40804820140c0402ebc2ec
Duvan Guillermo Ibarra Echeverry <guillermo2309@outlook.com>
2022-08-31 18:24:05 -0500

    Fix Linux Print Bug

--- linux-run.sh
+++ linux-run.sh
@@ -6,11 +6,11 @@ fi
 
 if [ $1 = "--sequential" ]
 then
-    printf 'Dimencion de la Matriz, Tiempo de ejecucion (s)' > sequential.csv
+    echo 'Dimencion de la Matriz, Tiempo de ejecucion (s)' > sequential.csv
     for mtx in 480 960 1920 3840 7680
     do
-        printf $mtx, $(./linux-matrix $mtx 1 --sequential) >> sequential.csv
-        printf "Done Matrix" $mtx"x"$mtx"."
+        echo $mtx, $(./linux-matrix $mtx 1 --sequential) >> sequential.csv
+        echo "Done Matrix" $mtx"x"$mtx"."
     done
     exit
 fi
@@ -20,32 +20,32 @@ then
     #printf "Setting maximum shared memory to 2GB. Requesting root permissions"
     #sudo sysctl -w kernel.shmmax=2147483648      
     #sudo sysctl -w kernel.shmall=524288
-    printf 'Cantidad de Procesos, Dimencion de la Matriz, Tiempo de ejecucion (s)' > process.csv
+    echo 'Cantidad de Procesos, Dimencion de la Matriz, Tiempo de ejecucion (s)' > process.csv
     for thread in 2 4 6 8 12 16
     do
-    	printf runing with $thread process.
+    	echo runing with $thread process.
         for mtx in 480 960 1920 3840 7680
         do
-            printf $thread, $mtx, $(./linux-matrix $mtx $thread --process) >> process.csv
-            printf "\tDone Matrix" $mtx"x"$mtx"."
+            echo $thread, $mtx, $(./linux-matrix $mtx $thread --process) >> process.csv
+            echo "\tDone Matrix" $mtx"x"$mtx"."
         done
-        printf "Done" $thread threads.
+        echo "Done" $thread threads.
     done
     exit
 fi
 
 if [ $1 = "--threading" ]
 then
-    printf 'Cantidad de Hilos, Dimencion de la Matriz, Tiempo de ejecucion (s)' > threading.csv
+    echo 'Cantidad de Hilos, Dimencion de la Matriz, Tiempo de ejecucion (s)' > threading.csv
     for thread in 2 4 6 8 12 16
     do
-    	printf runing with $thread threads.
+    	echo runing with $thread threads.
         for mtx in 480 960 1920 3840 7680
         do
-            printf $thread, $mtx, $(./linux-matrix $mtx $thread --threading) >> threading.csv
-            printf "\tDone Matrix" $mtx"x"$mtx"."
+            echo $thread, $mtx, $(./linux-matrix $mtx $thread --threading) >> threading.csv
+            echo "\tDone Matrix" $mtx"x"$mtx"."
         done
-        printf "Done" $thread threads.
+        echo "Done" $thread threads.
     done
     exit
 fi
