5c6def2def5778a51ed0abeb6555f930519f31c1
Duvan Guillermo Ibarra Echeverry <guillermo2309@outlook.com>
2022-08-31 16:18:01 -0500

    Implemented Shared Memory

--- mac-run.sh
+++ mac-run.sh
@@ -18,13 +18,9 @@ fi
 if [ $1 = "--process" ]
 then
     echo "Setting maximum shared memory to 2GB. Requesting root permissions"
-    if [ "$(uname)" == "Darwin" ]; then
-        sudo sysctl -w kern.sysv.shmmax=2147483648      
-        sudo sysctl -w kern.sysv.shmall=524288
-    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-        sudo sysctl -w kernel.shmmax=2147483648      
-        sudo sysctl -w kernel.shmall=524288
-    fi
+    sudo sysctl -w kern.sysv.shmmax=2147483648      
+    sudo sysctl -w kern.sysv.shmall=524288
+    
     echo 'Cantidad de Procesos, Dimencion de la Matriz, Tiempo de ejecucion (s)' > process.csv
     for thread in 2 4 6 8 12 16
     do
